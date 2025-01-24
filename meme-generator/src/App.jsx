501e8448293267a2d3b4aa4e18d354e3daeafc3a
roopeshbadri <badriroopesh@gmail.com>
2025-01-24 18:20:59 +0530

    UI Finished

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import Input from './components/Input'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Input/>
+    </>
+  )
+}
+
+export default App
